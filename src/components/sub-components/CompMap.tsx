24c7dd47a1e480171559252656d773357c251c2f
MAC <ahmed.hassaine432@gmail.com>
2025-07-29 17:50:46 +0100

    added slots

--- src/components/sub-components/CompMap.tsx
+++ src/components/sub-components/CompMap.tsx
@@ -1,32 +1,38 @@
 import { Slot } from "./CompSlot"
 
 export function Map() {
-  function Row() {
-    return <div className="flex gap-[16px]">
-      <Slot />
-      <Slot champion={"asdf"} />
-      <Slot />
-      <Slot />
-      <Slot />
-      <Slot />
-      <Slot />
-    </div>
-  }
+  const mapList = [
+    ["zoe", "zed", "yasuo", null, "graves", null, "master yi"],
+    ["zoe", "zed", "yasuo", null, "graves", null, "master yi"],
+    ["zoe", "zed", "yasuo", null, "graves", null, "master yi"],
+    ["zoe", "zed", "yasuo", null, "graves", null, "master yi"],
+    ["zoe", "zed", "yasuo", null, "graves", null, "master yi"]
+  ]
 
   return <div className="bg-primary center p-[27px] rounded-[14px] flex flex-col">
     <div className="flex flex-col gap-[-20px]">
-      <Row />
+      <Row championsList={mapList[0]} />
       <div className="flex gap-0 center m-[-12px]">
         <div className="w-[65px] h-4 bg-transparent"></div>
-        <Row />
+        <Row championsList={mapList[1]} />
       </div>
-      <Row />
+      <Row championsList={mapList[2]} />
       <div className="flex gap-0 center m-[-12px]">
         <div className="w-[65px] h-4 bg-transparent"></div>
-        <Row />
+        <Row championsList={mapList[3]} />
       </div>
     </div>
 
+
+  </div>
+}
+
+function Row({ championsList }: { championsList: Array<string | null> }) {
+  return <div className="flex gap-[16px]">
+
+    {championsList.map((name, i) =>
+      <Slot key={i} championName={name} />
+    )}
 
   </div>
 }
--- src/components/sub-components/CompSlot.tsx
+++ src/components/sub-components/CompSlot.tsx
@@ -1,16 +1,14 @@
 import Image from "next/image"
 import { StarIcon } from "@/components/icons/StarIcon"
-export function Slot({ champion }: { champion?: string }) {
+
+export function Slot({ championName }: { championName?: string | null }) {
   return <div className="relative">
     <div className="hexagon-bordered relative  w-[92px] h-[104px] bg-primary-light hexagon">
-
       <div className="w-[80px] h-[90px] absolute left-[6px] top-[7px] bg-secondary hexagon ">
-        {
-          champion && <Image src={'/zoe.png'} className="" width={85} height={95} alt="champion-slot" />
-        }
+        {championName && <Image src={`/${championName}.png`} className="" width={85} height={95} alt="champion-slot" />}
       </div>
     </div>
-    {champion &&
+    {championName &&
       <div className="bg-[#F6FF00]  rounded-[3px] py-[3px] center absolute gap-1 top-[8px]  px-1 left-[50%] transform translate-x-[-50%] ">
         <StarIcon className="text-black w-[15px] h-[15px]" />
         <StarIcon className="text-black w-[15px] h-[15px]" />
@@ -18,7 +16,7 @@ export function Slot({ champion }: { champion?: string }) {
         <StarIcon className="text-black w-[15px] h-[15px]" />
       </div>
     }
-    {champion &&
+    {championName &&
       <div className="absolute  gap-1 bottom-0 left-0  w-full">
         <Image src={'/item-example.png'} className="rounded-sm border-[3px] absolute bottom-[10px] right-1 border-secondary" height={26} width={26} alt="item" />
         <Image src={'/item-example.png'} className="rounded-sm border-[3px] absolute bottom-[0px] left-[50%] transform translate-x-[-50%] border-secondary" height={26} width={26} alt="item" />
